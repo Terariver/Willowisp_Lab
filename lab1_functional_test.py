7651e5f2757afc69f98989d492c5d5cabe8a2722
Terariver <hammerjack737@gmail.com>
2020-03-10 15:06:29 +0800

    Finished Functional Test

--- lab1_functional_test.py
+++ lab1_functional_test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -19,13 +20,25 @@ class NewVisitorTest(unittest.TestCase):
 		
 		# She sees a list containing three heroes with their corresponding 
 		# names, health points, and damage 
+		self.assertIn('Cloud',self.browser.body)
+		self.assertIn('Jester',self.browser.body)
+		self.assertIn('Sunflowey',self.browser.body)
+		self.assertIn('Health Points',self.browser.body)
+		self.assertIn('Damage',self.browser.body)
 		
 		# When she selects one of the heroes, she is sent to another page
 		# containing more information about the hero (additional stats, lore, image).
+		self.assertIn('Base Attack Damage',self.browser.body)
+		self.assertIn('Skills',self.browser.body)
+		self.assertIn('Lore',self.browser.body
 		
 		# She spots the page title and header mentions the name of the hero she selected.
+		self.assertIn("Detail - Jester", self.browser.title)
+		self.assertIn("Detail - Jester", self.browser.head)
 		
 		# While she is in a specific hero's page, she sees a button labeled "Back to Heroes List".
 		# She clicks this and she is redirected back to the wiki's homepage.	
-		
+		self.assertIn(button, self.browser.body)
+		self.assertIn("Back to Heroes List",self.browser.body.button)
+
 		self.fail('Finish the test!')
